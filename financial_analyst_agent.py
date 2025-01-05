664c2fa1d9b74374088a8b5d8da329804e1d8738
ahmedashfaq850 <ahmedashfaq6777@gmail.com>
2025-01-05 22:57:11 +0800

    add updated agent code

--- financial_analyst_agent.py
+++ financial_analyst_agent.py
@@ -1,51 +1,51 @@
 from phi.agent import Agent
 from phi.model.groq import Groq
-from phi.tools.yfinance import YFinanceTools
 from phi.tools.duckduckgo import DuckDuckGo
-import os
+from phi.tools.yfinance import YFinanceTools
+import os 
 from dotenv import load_dotenv
+load_dotenv
 
-load_dotenv()
 
-# web search agent 
+# first check if API Key is set or not
+if not os.getenv("GROQ_API_KEY"):
+    raise ValueError("GROQ_API_KEY not set in environment variables")
+
+# set api key in terminal
+#export GROQ_API_KEY="your-api-key"
+
+# Web Search Agent
+
 web_search_agent = Agent(
-  name = "web_search_agent",
-  role = "Search the Web for Financial Data",
-  model = Groq(id="llama3-groq-70b-8192-tool-use-preview"),
-  tools = [DuckDuckGo()],
-  instructions = ["Alway include sources"],
-  show_tools_calls = True,
-  markdown = True,
-)
-
-
-# Financial Analyst Agent
-
-financial_agent = Agent(
-    name = "financial_analyst_agent",
+    name = "web_search_agent",
     model = Groq(id="llama3-groq-70b-8192-tool-use-preview"),
-    tools=[YFinanceTools(
-        stock_price=True,
-        analyst_recommendations=True,
-        stock_fundamentals=True,
-        company_news=True,
-    )],
-    instructions = ["Use always table to show data"],
+    tools = [DuckDuckGo()],
+    instructions = ['Always include sources'],
     show_tools_calls = True,
     markdown = True,
+    debug_mode=True
 )
-  
-    
-# multi-agent
 
-multi_ai_agent = Agent(
-    team = [web_search_agent, financial_agent],
+# Finance Agent
+finance_agent = Agent(
+    name="Finance Agent",
     model = Groq(id="llama3-groq-70b-8192-tool-use-preview"),
-    instructions = ["Always include sources","Use table to display the data"],
-    show_tools_calls = True,
-    markdown = True, 
+    tools=[YFinanceTools(stock_price=True, analyst_recommendations=True, company_info=True, company_news=True)],
+    instructions=["Use tables to display data"],
+    show_tool_calls=True,
+    markdown=True,
 )
-    
 
-# Run the multi-agent
-multi_ai_agent.print_response("Summarize analyst recommendation and share the latest news for Facebook", stream = True)
+
+# Multi Agent
+multi_agent = Agent(
+    name="Multi Agent",
+    team=[web_search_agent, finance_agent],
+    model = Groq(id="llama3-groq-70b-8192-tool-use-preview"),
+    instructions=["Use tables to display data"],
+    show_tool_calls=True,
+    markdown=True,
+)
+
+# Print the response
+multi_agent.print_response("Tell me about the stocks of Tesla and Also tell me about the company", stream=True)
